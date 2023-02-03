bc80735bf073c24f294c1745e0440ce192c79e01
semen <semen.dorohov@yandex.ru>
2023-02-03 22:58:27 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿/// Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, 
+/// b1 k1 и b2 и k2 заданы
+
+void CrossPoint(double k1, double b1, double k2, double b2)
+{
+double x = (b1-b2)/(k2-k1);
+double y = k1 * x + b1;
+if(k1==k2) Console.Write("Заданные прямые не пересекаются!");
+else
+Console.Write($"Точка пересечения заданных прямых: ({x}; {y})");
+}
+
+Console.WriteLine("Input b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+CrossPoint(k1,b1,k2,b2);
